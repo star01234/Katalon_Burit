ea49caef9cf4fbdb7c100ba9a2879e2fc56f3464
star01234 <111554691+star01234@users.noreply.github.com>
2024-01-16 15:50:50 +0700

    Add files via upload

---
+++ MyProject/Libs/TempTestSuite1705392492057.groovy
@@ -0,0 +1,36 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+suiteProperties.put('id', 'Test Suites/Test Search By keyword')
+suiteProperties.put('name', 'Test Search By keyword')
+suiteProperties.put('description', '')
+suiteProperties.put('rerunTestFailImmediately', 'true')
+suiteProperties.put('retryCount', '3')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\Administrator\\Katalon Studio\\My First Web UI Project\\Reports\\25670116_150812\\Test Search By keyword\\25670116_150812\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/Test Search By keyword', suiteProperties, new File("C:\\Users\\Administrator\\Katalon Studio\\My First Web UI Project\\Reports\\25670116_150812\\Test Search By keyword\\25670116_150812\\testCaseBinding"))
---
+++ MyProject/Test Suites/Test Search By keyword.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Search By keyword</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bf75979c-28a4-4e39-beea-caccbb559df4</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb4f2548-90f5-4a3c-bf62-5e67c9216d45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC01 - Search By Keyword/TC0103 - Search By Keyword - PBRU</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>19e459c7-9d23-466d-883b-7e3fed1a2f48</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC01 - Search By Keyword/TC0102 - Search By Keyword - KRU</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2855bb2d-e7a1-48bb-978c-808fe9931847</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC01 - Search By Keyword/TC0101 - Search By Keyword - npru</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
